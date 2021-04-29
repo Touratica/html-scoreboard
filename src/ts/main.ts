cdff502ea9f2c35d6830b727006e403e68073f26
João Pedro Almeida <33964846+Touratica@users.noreply.github.com>
2021-04-29 14:28:46 +0100

    Implement color input

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -14,23 +14,35 @@ export function __init__() {
       e.preventDefault(); // Prevents page from reloading when form is submitted
       const formData = new FormData(form);
 
-      const homeInitials = document.querySelector("#home");
+      const homeInitials = document.querySelector<HTMLElement>("#home");
       if (homeInitials)
         homeInitials.innerHTML = formData.get("home-team-input") as string;
 
-      const homeScore = document.querySelector("#home-score");
+      const homeScore = document.querySelector<HTMLElement>("#home-score");
       if (homeScore)
         homeScore.innerHTML = formData.get("home-score-input") as string;
 
-      const awayTeam = document.querySelector("#away");
+      const homeColor = document.querySelector<HTMLElement>("#home-colors");
+      if (homeColor)
+        homeColor.style.backgroundColor = formData.get(
+          "home-color-input"
+        ) as string;
+
+      const awayTeam = document.querySelector<HTMLElement>("#away");
       if (awayTeam)
         awayTeam.innerHTML = formData.get("away-team-input") as string;
 
-      const awayScore = document.querySelector("#away-score");
+      const awayScore = document.querySelector<HTMLElement>("#away-score");
       if (awayScore)
         awayScore.innerHTML = formData.get("away-score-input") as string;
 
-      const timer = document.querySelector("#time");
+      const awayColor = document.querySelector<HTMLElement>("#away-colors");
+      if (awayColor)
+        awayColor.style.backgroundColor = formData.get(
+          "away-color-input"
+        ) as string;
+
+      const timer = document.querySelector<HTMLElement>("#time");
       if (timer) timer.innerHTML = formData.get("timer-input") as string;
       const time = String(formData.get("timer-input")).split(":");
       min = parseInt(time[0]);
@@ -74,7 +86,7 @@ function countdown() {
       sec += 60;
     }
 
-    const timer = document.querySelector("#time");
+    const timer = document.querySelector<HTMLElement>("#time");
     if (timer)
       timer.innerHTML = `${min}:${
         sec >= 10 ? Math.floor(sec) : `0${Math.floor(sec)}`
