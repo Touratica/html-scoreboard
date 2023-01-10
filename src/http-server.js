3685bc0f2d9e3ee77008029adf5b8f6aeaded54e
João Pedro Almeida <33964846+Touratica@users.noreply.github.com>
2023-01-10 03:50:09 +0000

    BREAKING CHANGE: turn into self-serving package (#92)
    * Bump husky from 8.0.1 to 8.0.3 (#90)
    
    * Bump postcss-loader from 7.0.1 to 7.0.2 (#85)
    
    * Bump autoprefixer from 10.4.12 to 10.4.13 (#87)
    
    * Bump postcss from 8.4.18 to 8.4.21 (#89)
    
    * Bump sass-loader from 13.1.0 to 13.2.0 (#88)
    
    * BREAKING CHANGE: turn into self-serving package (#91)
    
    * Change index.html location
    
    * Turn package into npx executable
    
    * Setup semantic-release
    
    * Bump postcss-loader from 7.0.1 to 7.0.2 (#85)
    
    * Bump autoprefixer from 10.4.12 to 10.4.13 (#87)
    
    * Bump postcss from 8.4.18 to 8.4.21 (#89)
    
    * Bump sass-loader from 13.1.0 to 13.2.0 (#88)
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Release version 1.1.1 (#83)
    
    * Update dependencies
    
    Bump autoprefixer from 10.4.12 to 10.4.13
    Bump css-loader from 6.7.1 to 6.7.3
    Bump husky from 8.0.1 to 8.0.2
    Bump lint-staged from 13.0.3 to 13.1.0
    Bump mini-css-extract-plugin from 2.6.1 to 2.7.2
    Bump postcss from 8.4.18 to 8.4.20
    Bump postcss-loader from 7.0.1 to 7.0.2
    Bump prettier from 2.7.1 to 2.8.1
    Bump sass from 1.55.0 to 1.57.1
    Bump sass-loader from 13.1.0 to 13.2.0
    Bump ts-loader from 9.4.1 to 9.4.2
    Bump typescript from 4.8.4 to 4.9.4
    Bump webpack from 5.74.0 to 5.75.0
    Bump webpack-cli from 4.10.0 to 5.0.1
    
    * Bump version from 1.1.0 to 1.1.1
    
    * Update dependabot.yml
    
    * Delete codeql-analysis.yml
    
    * Create release.yml
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ src/http-server.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+const http = require("http-server");
+const path = require("path");
+const port = 8080;
+
+const root = path.resolve(__dirname, "../dist");
+
+http
+  .createServer({ root: root }, function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World\n");
+  })
+  .listen(port);
+
+console.log(`Server running at http://localhost:${port}/`);
--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ const config = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "index.html",
+      template: "public/index.html",
     }),
 
     // Add your plugins here
